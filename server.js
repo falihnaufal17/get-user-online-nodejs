f226c1e787b2160352ee7ca951001ef2f85c06b5
Falih Naufal <dudu.kotkot@gmail.com>
2020-06-10 15:48:02 +0700

    count user online

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var count = 0;
+var $ipsConnected = [];
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket)=>{
+
+    var $ipAddress = socket.handshake.address;
+
+    if(!$ipsConnected.hasOwnProperty($ipAddress)){
+        $ipsConnected[$ipAddress] = 1;
+        count++;
+        io.emit('counter', {count: count})
+        console.log('count user: ' + count)
+    }
+
     console.log('a user connected');
 
     socket.on('chat message', (data)=>{
@@ -15,10 +27,18 @@ io.on('connection', (socket)=>{
     })
 
     socket.on('disconnect', ()=>{
+
+        if($ipsConnected.hasOwnProperty($ipAddress)){
+            delete $ipsConnected[$ipAddress];
+            count--;
+            io.emit('counter', {count: count})
+            console.log('count user: ' + count)
+        }
+        
         console.log('user disconnected')
     })
 })
 
-http.listen(process.env.PORT, () =>{
-    console.log('listening on *: 3000');
+http.listen(process.env.PORT || 3000, () =>{
+    console.log('listening on *: ' + process.env.PORT || 3000);
 })
